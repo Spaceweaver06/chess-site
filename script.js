54987178c741b09dedc86df834e26f201b2a49ac
Spaceweaver06 <75688416+Spaceweaver06@users.noreply.github.com>
2025-07-06 15:32:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
+const game = new Chess();
+
 const board = Chessboard('board', {
   draggable: true,
   position: 'start',
-  onDrop: onDrop
+  onDrop: (source, target) => {
+    const move = game.move({
+      from: source,
+      to: target,
+      promotion: 'q' // her zaman vezire terfi
+    });
+
+    if (move === null) return 'snapback';
+  }
 });
-
-const game = new Chess();
-
-function onDrop(source, target) {
-  const move = game.move({
-    from: source,
-    to: target,
-    promotion: 'q' // daima vezir terfisi
-  });
-
-  if (move === null) return 'snapback';
-}
